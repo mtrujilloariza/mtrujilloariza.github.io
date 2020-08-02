0fa466691af56ea1569298b5e8e10e512f2f3046
Marlon Trujillo Ariza <marlon.trujillo2001@gmail.com>
2020-08-01 20:17:34 -0700

    fixed onhover events for mobile

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var profilePicture = document.querySelector('#info .profile-picture img');
-console.log(profilePicture);
+
 
 function toggleIMG() {
-    //console.log(profilePicture.src);
     if (profilePicture.src.includes('pictures/profile-pic.jpg')) {
         profilePicture.src = 'pictures/memoji.png'
     } else {
         profilePicture.src = 'pictures/profile-pic.jpg'
     }
 }
+
+profilePicture.setAttribute('onclick', 'toggleIMG()');
